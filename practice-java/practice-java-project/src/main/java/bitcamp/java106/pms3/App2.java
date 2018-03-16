ec1a2af5dd2591d2b3bedb20240b84571f2d6633
Boram Choi <boram3510@gmail.com>
2018-03-16 11:01:18 +0900

    'test

--- practice-java/practice-java-project/src/main/java/bitcamp/java106/pms3/App2.java
+++ practice-java/practice-java-project/src/main/java/bitcamp/java106/pms3/App2.java
@@ -5,41 +5,10 @@ import bitcamp.java106.pms3.util.Console;
 import bitcamp.java106.pms3.controller.TeamController;
 import bitcamp.java106.pms3.controller.MemberController;
 
-
-import bitcamp.java106.pms3.domain.Board;
-
-public class Appb01 {
+public class App {
 
     static Scanner keyScan = new Scanner(System.in);
     static String option = null; // 명령어 뒤의 문자열
-
-
-    static Board[] boards = new Board[1000]; // 레퍼런스 생성
-    static int bCount = 0; // boards 등록 수    
-
-
-    // 게시글 등록 메서드
-    public static void onBoardAdd() {
-        Board board = new board(); // 인스턴스 생성
-                                // add명령어 실행할 때 마다 
-                                // team은 새로운 인스턴스가 생성된다.
-        System.out.println("[게시글 등록]");
-        System.out.print("제목? ");
-        board.name = keyScan.nextLine();
-        System.out.print("내용? ");
-        board.description = keyScan.nextLine();
-        System.out.print("등록일? ");
-        board.endDate = keyScan.nextLine();
-
-        boards[bCount++] = board;
-    }
-
-    // 게시글 조회 메서드
-
-
-
-
-
 
     // 종료인사
     static void onQuit() {
@@ -63,11 +32,13 @@ public class Appb01 {
     public static void main(String[] args) {
         TeamController.keyScan = keyScan;
         MemberController.keyScan = keyScan;
+        BoardController.keyScan = keyScan;
         Console.keyScan = keyScan;
 
-        System.out.println("실행 ㄱㄱㄱ");
+        
         while(true) {
-            String[] arr = prompt();
+            
+            String[] arr = Console.prompt();
             String menu = arr[0];
 
             if(arr.length == 2) { // 명령어 이외의 값이 있다면 다음 배열에 저장
@@ -77,32 +48,15 @@ public class Appb01 {
             }
 
             if(menu.equals("quit")) {
-                onquit();
+                onQuit();
                 break;
             } else if(menu.equals("help")) {
                 onHelp();
             } else if(menu.startsWith("team/")) {
-                TeamController.service(menu, optoin);
+                TeamController.service(menu, option);
             } else if(menu.startsWith("member/")) {
-                MemberController.service(menu, optoin);
-            } 
-            
-            else if(menu.equals("board/add")) {
-                onBoardAdd();
-            } else if(menu.equals("board/list")) {
-
-            } else if(menu.equals("board/view")) {
-
-            } else if(menu.equals("board/update")) {
-
-            } else if(menu.equals("board/delete")) {
-
-            }
-            
-            
-            
-            
-            else {
+                MemberController.service(menu, option);
+            } else {
                 System.out.println("명령어가 올바르지 않습니다.");
             }
 
